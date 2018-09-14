2eb22cc01f3ed05e59a3895bb33a78d0bc7fc744
Magnús Margeir Stefánsson <maggistef@gmail.com>
2018-09-14 09:43:19 +0000

    test: Test URL presence in allFeeds

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,11 +26,12 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
+        it('have a URL defined which is not empty', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url.length).not.toBe(0);
+            })
+        });
 
 
         /* TODO: Write a test that loops through each feed
